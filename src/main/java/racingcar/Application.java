ebd5147f62175f460fece0fde346255ee287849c
0xwb7 <woobin200111@gmail.com>
2025-10-23 15:44:16 +0900

    feat: Application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RacingController;
+
 public class Application {
     public static void main(String[] args) {
-
+        new RacingController().run();
+        Console.close();
     }
 }

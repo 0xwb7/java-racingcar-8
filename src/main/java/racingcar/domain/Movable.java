5d4d567fa47dea94c9ed7e91c9187932757c5dfe
0xwb7 <woobin200111@gmail.com>
2025-10-27 22:09:11 +0900

    refactor: 매직 넘버 상수화

--- src/main/java/racingcar/domain/Movable.java
+++ src/main/java/racingcar/domain/Movable.java
@@ -1,10 +1,12 @@
 package racingcar.domain;
 
 public final class Movable {
+    private static final int MOVABLE_NUMBER = 4;
+
     private Movable() {
     }
 
     public static boolean isMovable(int random) {
-        return random >= 4;
+        return random >= MOVABLE_NUMBER;
     }
 }
--- src/main/java/racingcar/util/RandomNumber.java
+++ src/main/java/racingcar/util/RandomNumber.java
@@ -3,10 +3,12 @@ package racingcar.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public final class RandomNumber {
+    private static final int RANDOM_MIN_NUM = 0;
+    private static final int RANDOM_MAX_NUM = 9;
     private RandomNumber() {
     }
 
     public static int pickRandomNumber() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(RANDOM_MIN_NUM, RANDOM_MAX_NUM);
     }
 }

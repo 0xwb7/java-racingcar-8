19e788b594ec192bff0afd1c49a1df42537bb4d3
0xwb7 <woobin200111@gmail.com>
2025-10-27 21:53:34 +0900

    chore: Application 클래스 내 catch 삭제 - catch

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,14 +2,11 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.RacingController;
-import racingcar.exception.RacingException;
 
 public class Application {
     public static void main(String[] args) {
         try {
             new RacingController().run();
-        } catch (RacingException e) {
-            System.out.println(e.getMessage());
         } finally {
             Console.close();
         }

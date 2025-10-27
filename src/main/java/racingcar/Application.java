fea29c90132fcc8c127b03c1dec4cf7dcb573044
0xwb7 <woobin200111@gmail.com>
2025-10-27 21:41:52 +0900

    refactor: try-catch-final문 사용 - 예외처리가 되면 Console.close()가 실행이 안 되기 때문에 try-catch-finally 를 사용하여 예외처리가 되어도 Console이 닫힐 수 있도록 함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,16 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.RacingController;
+import racingcar.exception.RacingException;
 
 public class Application {
     public static void main(String[] args) {
-        new RacingController().run();
-        Console.close();
+        try {
+            new RacingController().run();
+        } catch (RacingException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            Console.close();
+        }
     }
 }
